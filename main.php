e42311e8cc8d1075d426b6ceb763a999d94c37f4
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-11-11 17:15:11 +0100

    Adding some key elements
    Adding: Navbar, header, and the first main component. Although no functionallity added yet.

---
+++ main.php
@@ -0,0 +1,15 @@
+<main>
+    <div class="bg-black">
+        <div class="produkt-wrapper">
+            <div class="produkt-text-header">
+                <h1>3d PRODUKT BILD</h1>
+            </div>
+            <div class="produkt-text-bread">
+                <p>New launch 2024 3D produktbild</p>
+            </div>
+        </div>
+    </div>
+    <div class="bg-checkers">
+
+    </div>
+</main>
---
+++ navbar.php
@@ -0,0 +1,10 @@
+<nav class="nav-bar">
+    <div class="logo">
+        <img src="assets/PT Logo.svg" />
+    </div>
+    <div class="bar">
+        <a class="navbar-anchor" href="#"><p>Produkter</p></a>
+        <a class="navbar-anchor"  href="#"><p>Om</p><p> oss</p></a>
+        <a class="navbar-anchor"  href="#"><p>Kontakt</p></a>
+    </div>
+</nav>

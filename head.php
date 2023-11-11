b2e9300d8715a10568d68d75dcaf727b9228521e
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-11-11 12:29:51 +0100

    Initial commit
    Added basic files for the landing site

---
+++ head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="Description"    content="Landing page for Protech-launch site">
+        <meta property="og:title"       content="Protech">
+        <meta property="og:description" content="Product launching page and information">
+        <title>Protech</title>
+        <link rel="stylesheet" href="style.css"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Trispace:wght@700;800&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
+    </head>
+    <body>

6ae2841dd1c9956bd350df176b3723eea7cdb212
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-11-12 14:50:25 +0100

    adding some style to testproduct

--- data.php
+++ data.php
@@ -4,7 +4,7 @@ $productArray =
     "produkt-namn1" => 
     [
         "produkt-namn" => "produkt-namn1",
-        "produkt-bild" => "assets/product_img/placeholder_product1.png",
+        "produkt-bild" => "assets/product_img/Placeholder_product1.svg",
         "produkt-pris" => "produkt-pris1",
         "produkt-beskrivning" => "Här kan du välja mellan olika typer av pedaler och andra saker mm",
         "produkt-kategori" => "produkt-kategori1",
@@ -14,7 +14,7 @@ $productArray =
     "produkt-namn2" => 
     [
         "produkt-namn" => "produkt-namn2",
-        "produkt-bild" => "assets/product_img/placeholder_product2.png",
+        "produkt-bild" => "assets/product_img/Placeholder_product2.svg",
         "produkt-pris" => "produkt-pris2",
         "produkt-beskrivning" => "Här kan du välja mellan olika typer av pedaler och andra saker mm",
         "produkt-kategori" => "produkt-kategori2",
@@ -24,7 +24,7 @@ $productArray =
     "produkt-namn3" => 
     [
         "produkt-namn" => "produkt-namn3",
-        "produkt-bild" => "assets/product_img/placeholder_product3.png",
+        "produkt-bild" => "assets/product_img/Placeholder_product3.svg",
         "produkt-pris" => "produkt-pris3",
         "produkt-beskrivning" => "Här kan du välja mellan olika typer av pedaler och andra saker mm",
         "produkt-kategori" => "produkt-kategori3",
@@ -34,7 +34,7 @@ $productArray =
     "produkt-namn4" => 
     [
         "produkt-namn" => "produkt-namn4",
-        "produkt-bild" => "assets/product_img/placeholder_product4.png",
+        "produkt-bild" => "assets/product_img/Placeholder_product4.svg",
         "produkt-pris" => "produkt-pris4",
         "produkt-beskrivning" => "Här kan du välja mellan olika typer av pedaler och andra saker mm",
         "produkt-kategori" => "produkt-kategori4",
--- test-products.php
+++ test-products.php
@@ -6,12 +6,12 @@
         foreach ($productArray as $product){?>
         <div class="product-cards">
             <div class="product-card-text">
-                <div class="product-card-product-name"><?= $product['produkt-namn'] ?></div>
+                <div class="product-card-product-name"><p><?= $product['produkt-namn'] ?></p></div>
                 <p class="product-card-p"><?= $product['produkt-beskrivning'] ?></p>
                 <button class="product-card-button">Se produkter</button>
             </div>
             <div class="product-card-img">
-                <img src="<?= $product['produkt-bild'] ?>" alt="product-image">
+                <img class="product-img" src="<?= $product['produkt-bild'] ?>" alt="product-image">
             </div>
         </div><?php
         }?>

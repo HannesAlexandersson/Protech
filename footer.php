044c210cc7290d06a5efc1b1a6a954e5e121ac77
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-11-27 10:19:41 +0100

    adding responsive features to navbar
    both mobile and desktop navbar gets hoover on effects to the search bar and the expandable hamburger bars

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 </body>
 <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+<script src="script.js"></script>
     <footer>
         <div class="footer-wrapper">                
             <div class="left-footer-column">
--- navbar.php
+++ navbar.php
@@ -8,7 +8,12 @@
         <a class="navbar-anchor"  href="#"><p>Kontakt</p></a>
     </div>
     <div class="bars">
-        <img class="hamburger" src="assets/hero/nav/Search Icon.png" />
+        <div class="dropdown">
+            <img onclick="myFunction()" class="search" src="assets/hero/nav/Search Icon.png" />
+            <div id="myDropdown" class="dropdown-content">
+                <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+            </div>
+        </div>
         <img class="lang" src="assets/hero/nav/Language icon.png" />
     </div>
 </nav>
@@ -17,6 +22,11 @@
         <img class="logo-mobile" src="assets/logotypes/mobilLogo.png" />
     </div>
     <div class="bars-mobile">
-        <img class="hamburger" src="assets/Designsystem/PhonehamburgerBars.png" />        
+        <img class="hamburger" src="assets/Designsystem/PhonehamburgerBars.png" />   
+        <div class="drop-down-content">
+            <a class="navbar-anchor" href="#"><p>Produkter</p></a>
+            <a class="navbar-anchor"  href="#"><p>Om oss</p></a>
+            <a class="navbar-anchor"  href="#"><p>Kontakt</p></a>
+        </div>     
     </div>
 </nav>

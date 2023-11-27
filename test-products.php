167ae5979262f0693238d00fe231bc9211dc3519
Cappe <casper@sundegard.st>
2023-11-27 17:48:31 +0100

    db conversion + lite annat

--- test-products.php
+++ test-products.php
@@ -1,29 +1,30 @@
-    <content>  
-        <div class="sec-product-box"><?php 
-            foreach ($productArray as $product){?>
-                <div class="sec-product <?=$product['produkt-class']?>">
-                    <div class="border-box-with-corner">
-                        <div class="sec-prod-text-box">
-                            <h4><?= $product['produkt-namn'] ?></h4>
-                            <p class="model-bread-text"><?= $product['produkt-beskrivning'] ?></p>                    
-                        </div>
+<content>  
+    <div class="sec-product-box">
+        <?php require 'backend/dbLoad.php'; ?>
+        <?php foreach ($proInfo as $product){ ?>
+            <div class="sec-product <?= $product['class']; ?>">
+                <div class="border-box-with-corner">
+                    <div class="sec-prod-text-box">
+                        <h4><?= $product['name']; ?></h4>
+                        <p class="model-bread-text"><?= $product['desc']; ?></p>
                     </div>
-                </div>              
-            <?php }?>  
-        </div>         
-    </content>
-    <div class="cont-carusel">
-        <div class="outer-border-box">
-            <div class="gallery js-flickity"
-                data-flickity-options='{ "cellAlign": "left", "contain": true }'><?php
-                foreach ($productArray as $product){?>
-                <div class="gallery-cell <?=$product['produkt-class-mob']?>">
-                    <div class="corners">                    
-                        <h4><?= $product['produkt-namn'] ?></h4>
-                        <p><?= $product['produkt-beskrivning'] ?></p>
-                    </div>                
-                </div><?php
-                }?>
+                </div>
             </div>
+        <?php } ?>
+    </div>  
+</content>
+<div class="cont-carusel">
+    <div class="outer-border-box">
+        <div class="gallery js-flickity"
+            data-flickity-options='{ "cellAlign": "left", "contain": true }'>
+            <?php foreach ($proInfo as $product){ ?>
+                <div class="gallery-cell <?= $product['class_mob']; ?>">
+                    <div class="corners">                    
+                        <h4><?= $product['name']; ?></h4>
+                        <p><?= $product['desc']; ?></p>
+                    </div>                
+                </div>
+            <?php } ?>
         </div>
     </div>
+</div>

0ce46e37a263d2702b28871cd74e371c01d3b2c6
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-11-29 14:00:56 +0100

    added carusel all pics

--- detail.php
+++ detail.php
@@ -5,15 +5,13 @@
         </div>        
         <div class="outer-border-box detail-container">            
             <div class="second-carousel js-flickity" data-flickity='{"cellAlign": "left", "contain": true, "autoPlay": true}'>
-                <div class="detail-image-wrapper second-carosel-cell img1">
-                    
-                </div>
-                <div class="detail-image-wrapper second-carosel-cell img2">
-                    
-                </div>
-                <div class="detail-image-wrapper second-carosel-cell img3">
-                    
-                </div>
+                <div class="detail-image-wrapper second-carosel-cell img1"></div>
+                <div class="detail-image-wrapper second-carosel-cell img2"></div>
+                <div class="detail-image-wrapper second-carosel-cell img3"></div>
+                <div class="detail-image-wrapper second-carosel-cell img4"></div>
+                <div class="detail-image-wrapper second-carosel-cell img5"></div>
+                <div class="detail-image-wrapper second-carosel-cell img6"></div>
+                <div class="detail-image-wrapper second-carosel-cell img7"></div>
             </div>
         </div>        
     </div>

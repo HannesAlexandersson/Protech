e330b3dc8512c5c7703f85de80488cfcc614b9c1
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-11-29 17:12:05 +0100

    added scrolling behaviur to mobile aswell

--- script.js
+++ script.js
@@ -24,19 +24,23 @@ function filterFunction() {
 $(function(){
   var lastScrollTop = 0;
   var navHeight = $('.nav-bar').outerHeight();
+  var mobileNavHeight = $('.nav-bar-mobile').outerHeight();
 
   $(window).scroll(function(){
     var scrolled = $(document).scrollTop();
 
-    if(scrolled > navHeight){
+    if(scrolled > navHeight || scrolled > mobileNavHeight){
       if (scrolled > lastScrollTop){        
         $('.nav-bar').removeClass('sticky').addClass('animate');
+        $('.nav-bar-mobile').removeClass('sticky').addClass('animate');
       } else {        
         $('.nav-bar').removeClass('animate').addClass('sticky');
+        $('.nav-bar-mobile').removeClass('animate').addClass('sticky');
       }
     }
     else{      
       $('.nav-bar').removeClass('animate sticky');
+      $('.nav-bar-mobile').removeClass('animate sticky');
     }
 
     lastScrollTop = scrolled;

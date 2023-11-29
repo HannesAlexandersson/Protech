22d53cf817374a61ce44e00bdf6ff955502a2081
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-11-29 11:44:16 +0100

    added second carosel

--- detail.php
+++ detail.php
@@ -2,20 +2,19 @@
     <div class="detail-wrapper">
         <div class="about-us free-float-cornerBox">
             <h4 class="mobil-h4">DETALJ-VY</h4>
-        </div>
-        <div class="outer-border-box detail-container">
-            <div class="detail-image-wrapper desktop-hide">
-                <img src="assets/product_img/detail/Closeup_front_side 1.png" alt="closeup-image of sequenser" class="detail-image free-float-cornerBox" />
+        </div>        
+        <div class="outer-border-box detail-container">            
+            <div class="second-carousel js-flickity" data-flickity='{"cellAlign": "left", "contain": true, "autoPlay": true}'>
+                <div class="detail-image-wrapper second-carosel-cell img1">
+                    
+                </div>
+                <div class="detail-image-wrapper second-carosel-cell img2">
+                    
+                </div>
+                <div class="detail-image-wrapper second-carosel-cell img3">
+                    
+                </div>
             </div>
-            <div class="detail-image-wrapper for-the-empire">
-                <img src="assets/product_img/detail/Closeup_front_side 12.png" alt="closeup-image of sequenser" class="detail-image free-float-cornerBox" />
-            </div>
-            <div class="detail-image-wrapper for-the-empire">
-                <img src="assets/product_img/detail/Back_side 1.png" alt="closeup-image of sequenser" class="detail-image free-float-cornerBox" />
-            </div>
-            <div class="detail-image-wrapper  for-the-empire">
-                <img src="assets/product_img/detail/Vector.png" alt="closeup-image of back of sequenser" class="detail-image free-float-cornerBox" />
-            </div>
-        </div>
+        </div>        
     </div>
 </div>
